742edcf6e2932248f3b69b52b65d9c1cef23fca9
user1 <user1@A518-08>
2021-05-21 15:28:46 +0300

    Fixed Method one Interface

--- BottleWebProject_ С821_ 2_ SIT/app.py
+++ BottleWebProject_ С821_ 2_ SIT/app.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static').replace('\\', '/')
     HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
+    try: 
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
